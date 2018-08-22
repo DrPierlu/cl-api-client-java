39f6961710652446dbe03314f2502e5b03bc61bf
IG01755 <IG01755@N01688.corp.infogroup.it>
2018-08-22 11:25:16 +0200

    Fix: List from java.util package

--- modules/cl-client-codegen/src/main/java/io/commercelayer/api/codegen/service/generator/ServiceGeneratorUtils.java
+++ modules/cl-client-codegen/src/main/java/io/commercelayer/api/codegen/service/generator/ServiceGeneratorUtils.java
@@ -1,8 +1,8 @@
 package io.commercelayer.api.codegen.service.generator;
 
-import java.awt.List;
 import java.lang.annotation.Annotation;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
